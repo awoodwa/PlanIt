603b611e6d8a2f3de9223ccb1d787e787c6b8164
mdbresh <mdbresh@uw.edu>
2020-03-12 10:34:16 -0700

    updated wrapper to include chart output

--- wrapper.py
+++ wrapper.py
@@ -8,7 +8,7 @@ import wind_energy
 import solar_handling
 
 import pandas as pd
-import h5pyd
+import numpy as np
 
 # wtk = h5pyd.File('nrel/wtk-us.h5', 'r')
 
@@ -54,7 +54,7 @@ def wrapper(wtk, city, state, land_available, goal = 100, residential = False,\
             energy_goal = (goal/100) * energy_use
 
         # only solar will be used for residences
-        solar = solar_handling.annual_solar_mean(wtk, dataset_loc[0], dataset_loc[1])
+        solar = solar_handeling.annual_solar_mean(wtk, dataset_loc[0], dataset_loc[1])
         # if solar resources are greater than their goal
         if solar >= energy_goal:
             energy_output = energy_goal
@@ -78,21 +78,62 @@ def wrapper(wtk, city, state, land_available, goal = 100, residential = False,\
         turbines = round(land_available / 0.4)
         wind_cost = cost_handling.cost_of_wind(turbines)
 
-        solar_raw = solar_handling.annual_solar_mean(wtk, dataset_loc)
+        solar_raw = solar_handeling.annual_solar_mean(wtk, dataset_loc)
         panels = round(land_available * 1e6 / 1.635481)
         solar = panels * solar_raw
         solar_cost = cost_handling.cost_of_solar(solar_raw) * panels
 
-        data = pd.DataFrame()
+        solar_panels = []
 
-        for p in range(panels):
+        for p in range(round(panels/100000)):
+            wind_turbines = []
             for t in range(turbines):
                 c = cost_handling.cost_of_wind(t) +\
-                    cost_handling.cost_of_solar(p)
-                se = p * solar_raw
+                        cost_handling.cost_of_solar(p * 100000)
+                se = p * solar_raw / 1e6
                 we = t * wind
                 e = se + we
-                data[p][t] = {'panels':p, 'turbines':t,
-                              'cost':c, 'energy output':e}
+                wind_turbines.append([p * 100000, t, round(c, 2), round(e, 2)])
+            solar_panels.append(wind_turbines)
 
-        return data 
+        #data = pd.DataFrame(solar_panels)
+
+        x = range(round(panels/100000))
+        y = np.linspace(turbines, 0, round(panels/100000))
+        limit = pd.DataFrame({'x':x, 'y':y})
+
+        p, t, = np.meshgrid(range(0, round(panels/100000), range(0, turbines)))
+        e = (p*100000*solar_raw/1e6) + (t*wind)
+        c = (cost_handling.cost_of_wind(t) +\
+                cost_handling.cost_of_solar(p * 100000))/1000000
+
+        data = pd.DataFrame({'Panels': p.ravel(), 'Turbines': t.ravel(),\
+                            'e': e.ravel(), 'Cost': c.ravel()})
+
+        alt.data_transformers.disable_max_rows()
+        chart = alt.Chart(data2).mark_point().encode(
+            alt.X('Panels:Q',
+                  scale = alt.Scale(domain = (0, panels/100000),
+                                    clamp = True
+                                   ),
+                  axis = alt.Axis(title = 'Number of Solar Panels (100,000)')
+                 ),
+            alt.Y('Turbines:Q',
+                  scale = alt.Scale(domain = (0, turbines),
+                                    clamp = True
+                                   ),
+                  axis = alt.Axis(title = 'Number of Wind Turbines')
+                 ),
+            color = alt.Color('e:Q',
+                              legend = alt.Legend(title = 'Energy Output (MWh)'),
+                             scale = alt.Scale(scheme = 'viridis')),
+            tooltip = ['Panels:Q', 'Turbines:Q', 'Cost:N']
+        )
+
+        line = alt.Chart(limit).mark_line().encode(
+            x = 'x:Q',
+            y = 'y:Q',
+            color = alt.value('black')
+        )
+        alt.layer(
+            chart, line).configure_axis(grid = False)
