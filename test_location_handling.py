4f3c3f9266708fabb6fbd847946e0309bf4c558b
mdbresh <mdbresh@uw.edu>
2020-03-14 13:01:02 -0700

    tests for cost, location, energy use, and wrapper

---
+++ test_location_handling.py
@@ -0,0 +1,56 @@
+import location_handling
+import h5pyd
+import numpy as np
+
+
+def test_get_loc():
+    '''
+    This function tests the get_loc(city, state) function to make sure the
+    output is a tuple containing floats and that the output matches the
+    working state output of inputs 'Salem' and 'OR'.
+    '''
+    test = location_handling.get_loc('Salem', 'OR')
+    result = (44.9232, -123.0245)
+
+    assert type(test) == tuple,\
+        'Test result type (%s) is not of type tuple.' % str(type(test))
+
+    assert (test[0] == result[0]) & (test[1] == result[1]),\
+        'Test result (%s) does not contain expected values (%s).' %\
+        (str(test), str(result))
+
+
+def test_wtk_locator():
+    '''
+    This function tests the wtk_locator(wtk, location) function to make sure
+    the output is a tuple containing integers that match the indices output
+    of Salem, OR latitude and longitude coordinates.
+    '''
+    wtk = h5pyd.File('/nrel/wtk-us.h5', 'r')
+    salem = (44.9232, -123.0245)
+    test = location_handling.wtk_locator(wtk, salem)
+    result = (1320, 480)
+
+    assert type(test) == tuple,\
+        'Test result type (%s) is not of type tuple.' % str(type(test))
+
+    assert (test[0] == result[0]) & (test[1] == result[1]),\
+        'Test result (%s) does not contain expected values (%s).'\
+        % (str(test), str(result))
+
+
+def test_get_pop():
+    '''
+    This function tests the get_pop(city, state) function to make sure the
+    output is an integer and matches the known value of Salem, OR.
+    '''
+    salem = (44.9232, -123.0245)
+    test = location_handling.get_pop(salem)
+    result = np.int64(259816)
+
+    assert type(test) == type(result),\
+        'Test result type (%s) is not of type integer.' % str(type(test))
+
+    assert test == result,\
+        'Test result (%s) is not equal to expected value.'\
+        % (str(test), str(result))
---
+++ test_wrapper.py
@@ -0,0 +1,36 @@
+import wrapper
+import h5pyd
+import altair as alt
+
+
+def test_wrapper():
+    '''
+    This function tests the functionality of the wrapper module.
+    '''
+    wtk = h5pyd.File('/nrel/wtk-us.h5', 'r')
+    test_gov =\
+        wrapper.wrapper(wtk, 'Salem', 'OR', land_available=20, goal=80)
+
+    assert type(test_gov) == alt.vegalite.v4.api.LayerChart,\
+        'Test result type (%s) is not of type (%s).'\
+        % (str(test_gov), 'alt.vegalite.v4.api.LayerChart')
+
+    # now to test the residential branch
+    test_res =\
+        wrapper.wrapper(wtk, 'Salem', 'OR', 100000, goal=100,
+                        residential=True, household_size=8)
+
+    result = [149129.03, 307.0, 53454.93, 12144.06, 8.13, 4353.01]
+
+    assert type(test_res) == type(result),\
+        'Test result type (%s) is not of type list/array.'\
+        % str(type(test_res))
+
+    assert len(test_res) == len(result),\
+        'Test result length (%s) is not the expected length (%s).'\
+        % (str(len(test_res), len(result)))
+
+    for i in range(6):
+        assert test_res[i] == result[i],\
+            'Entry in test result list (%s) is not equal to expected result\
+            (%s).' % (str(test_res[i]), str(result[i]))
