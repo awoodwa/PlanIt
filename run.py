b13616546e2d617a6945c7ba3750312886444e0c
mdbresh <mdbresh@uw.edu>
2020-03-16 12:42:00 -0700

    cleaning branch up

--- run.py
+++
@@ -1,7 +0,0 @@
-import os
-from PlanIt import planitapp
-
-
-if __name__ == "__planitapp__":
-    #port = int(os.environ.get("PORT", 5000))
-    planitapp.app.run(debug=True)  #(host="0.0.0.0", port=port)
