a99aefe6e615a0fb5039929140e2d40f6fbb4475
Laura Jacoby <ljacoby@uw.edu>
2020-03-16 12:14:13 -0700

    made changes to import of wind_energy.py

--- PlanIt/tests/test_wind_energy.py
+++ PlanIt/tests/test_wind_energy.py
@@ -1,13 +1,11 @@
 import numpy as np
 import h5pyd
-wind_energy import power_eff, weibull_coeff, weibull_pdf, summation, \
-                        wind_energy_output, create_tseries, aeo_average, \
-                        wind_landuse
+from .. import wind_energy
 
 
 wtk = h5pyd.File("/nrel/wtk-us.h5", 'r')
 NYC_idx = (1044, 2378)
-NYC08_tseries = create_tseries(wtk, NYC_idx)[1]
+NYC08_tseries = wind_energy.create_tseries(wtk, NYC_idx)[1]
 
 
 def test_power_eff():
@@ -17,7 +15,7 @@ def test_power_eff():
     '''
     x_test = 14.2
 
-    result_cp = power_eff(x_test)
+    result_cp = wind_energy.power_eff(x_test)
 
     assert result_cp == 0.22110923757796938, \
         'Result value is not same as expected'
@@ -30,7 +28,7 @@ def test_weibull_coeff():
     Test function for weibull_coeff. Checks the output values are of the
     correct type, and that a only a tuple of 2 values is returned (k and c).
     '''
-    result_coeff = weibull_coeff(NYC08_tseries)
+    result_coeff = wind_energy.weibull_coeff(NYC08_tseries)
 
     assert isinstance(result_coeff[1], np.float64), \
         'Result shape parameter is not the type expected'
@@ -46,14 +44,14 @@ def test_weibull_pdf():
     test_windsp = 4.0
     test_k, test_c = (2.095213510638354, 6.873579224294875)
 
-    result_pdf = weibull_pdf(test_windsp, test_k, test_c)
+    result_pdf = wind_energy.weibull_pdf(test_windsp, test_k, test_c)
 
     assert result_pdf <= 0.15, \
         'Result PDF for wind speed is higher than allowed'
 
     tot_pdf = []
     for i in np.arange(0, 26, 1.0):
-        val = weibull_pdf(i, 2.095213510638354, 6.873579224294875)
+        val = wind_energy.weibull_pdf(i, 2.095213510638354, 6.873579224294875)
         tot_pdf.append(val)
     result_total = sum(tot_pdf)
 
@@ -66,7 +64,7 @@ def test_summation():
     Test function for summation. Checks that output is of the correct type
     and has the correct value.
     '''
-    result_summation = summation(NYC08_tseries)
+    result_summation = wind_energy.summation(NYC08_tseries)
 
     assert isinstance(result_summation, float), \
         'Result summation value is not of type float'
@@ -81,7 +79,7 @@ def test_wind_energy_output():
     an unexpectedly low value.
     '''
 
-    result_output = wind_energy_output(NYC08_tseries)
+    result_output = wind_energy.wind_energy_output(NYC08_tseries)
 
     assert result_output >= 7439, \
         'Result energy output is lower than expected'
@@ -92,7 +90,7 @@ def test_create_tseries():
     Test function for create_tseries. Checks that the output is a list
     and the values in the list are numpy array.
     '''
-    result_tseries = create_tseries(wtk, NYC_idx)
+    result_tseries = wind_energy.create_tseries(wtk, NYC_idx)
 
     assert type(result_tseries) == list, \
         'Time series result type is not of type list'
@@ -106,7 +104,7 @@ def test_aeo_average():
     than the smallest value in the series that make up the average. Also,
     a check that it is of the right type.
     '''
-    result_average = aeo_average(wtk, NYC_idx)
+    result_average = wind_energy.aeo_average(wtk, NYC_idx)
 
     assert np.all(result_average > NYC08_tseries), \
         'Result AEO average is lower than expected'
@@ -122,7 +120,7 @@ def test_wind_landuse():
     '''
     land_available = 12
 
-    result_max_energy = wind_landuse(land_available, wtk, NYC_idx)
+    result_max_energy = wind_energy.wind_landuse(land_available, wtk, NYC_idx)
 
     assert isinstance(result_max_energy, float), \
         'Result max energy is not of type float'
